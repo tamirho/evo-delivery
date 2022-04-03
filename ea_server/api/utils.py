29958ce318e0f139bb38afd14d142db9eedf001a
vladib6 <vladib6@gmail.com>
2022-04-03 20:10:11 +0300

    format code

--- ea_server/api/utils.py
+++ ea_server/api/utils.py
@@ -1,20 +1,20 @@
 import math
 from operator import itemgetter
-def transform(result, data):
-    result_as_dict = dict()
-    for driver in data["drivers"]:
-        result_as_dict[driver["id"]]= []
 
-    for order_index,number in enumerate(result):
+
+def transform(result, data):
+    result_as_dict = {driver["id"]:[] for driver in data["drivers"]}
+
+    for order_index, number in enumerate(result):
         driver_index = math.floor(number)
         driver_id = data["drivers"][driver_index]["id"]
-        item=(data["orders"][order_index]["id"],number)
+        item = (data["orders"][order_index]["id"], number)
         result_as_dict[driver_id].append(item)
 
     for driver in result_as_dict:
         print(result_as_dict[driver])
-        result_as_dict[driver]=sorted(result_as_dict[driver], key=itemgetter(1))
-        result_as_dict[driver]= [i[0] for i in result_as_dict[driver]]
+        result_as_dict[driver] = sorted(result_as_dict[driver], key=itemgetter(1))
+        result_as_dict[driver] = [i[0] for i in result_as_dict[driver]]
 
     return result_as_dict
 
@@ -24,17 +24,17 @@ def transform(result, data):
 #   "driver_id3":[orderId , orderId,...]
 # }
 # The object with current data:
-#{
+# {
 #  "11": [
-#    "4", 
+#    "4",
 #    "1"
-#  ], 
+#  ],
 #  "12": [
-#    "5", 
+#    "5",
 #    "6"
-#  ], 
+#  ],
 #  "13": [
-#    "3", 
+#    "3",
 #    "2"
 #  ]
-#}
+# }
