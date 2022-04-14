63ec99a199fdc79590b7c47745fe09d7b1093c9b
tamirho <tamirh1994@gmail.com>
2022-04-14 12:58:46 +0300

    remove tmp ea-engine

--- ea-engine/mut.py
+++
@@ -1,8 +0,0 @@
-from deap import tools
-
-class Mutates():
-    SHUFFLE = tools.mutShuffleIndexes
-
-    @classmethod
-    def get_default(cls):
-        return cls.SHUFFLE, {"indpb": 0.05}
--- ea-engine/sel.py
+++
@@ -1,8 +0,0 @@
-from deap import tools
-
-class Selection():
-    TOURNAMENT = tools.selTournament
-
-    @classmethod
-    def get_default(cls):
-        return cls.TOURNAMENT, {"tournsize": 3}
