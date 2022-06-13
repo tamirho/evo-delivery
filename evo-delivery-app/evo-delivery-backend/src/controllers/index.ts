3a50a5c8e0f23b533185f54bb5381541bf6b6746
vladib6 <vladib6@gmail.com>
2022-06-13 15:53:48 +0300

    draft controller, service , db schema

--- evo-delivery-app/evo-delivery-backend/src/controllers/index.ts
+++ evo-delivery-app/evo-delivery-backend/src/controllers/index.ts
@@ -1,4 +1,5 @@
 export * as driverController from './driver.controller';
 export * as orderController from './order.controller';
+export * as draftController from './draft.controller';
 export * as routeController from './route.controller';
 export * as eaEngineController from './ea-engine.controller';
--- evo-delivery-app/evo-delivery-backend/src/routes/v1/index.ts
+++ evo-delivery-app/evo-delivery-backend/src/routes/v1/index.ts
@@ -3,11 +3,14 @@ import { orderRouter } from './order.route';
 import { driverRouter } from './driver.route';
 import { routeRouter } from './route.route';
 import { eaEngineRouter } from './ea-engine.route';
+import { draftRouter } from './draft.route';
 
 const router = express.Router({ mergeParams: true });
 router.use('/orders', orderRouter);
 router.use('/drivers', driverRouter);
 router.use('/routes', routeRouter);
 router.use('/ea-engine', eaEngineRouter)
+router.use('/drafts', draftRouter);
+
 
 export { router as v1Router };
