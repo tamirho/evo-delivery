42a6fe124262e3d7e5176fd46d16e60cf3f5138a
vladib6 <vladib6@gmail.com>
2022-08-12 11:53:21 +0300

    driver form

--- evo-delivery-app/evo-delivery-frontend/src/pages/drivers/components/CreateDriver/CreateDriver.tsx
+++ evo-delivery-app/evo-delivery-frontend/src/pages/drivers/components/CreateDriver/CreateDriver.tsx
@@ -1,16 +1,19 @@
-import { EvoForm } from "../EvoForm";
+import { useCreateEntity } from "../../../../hooks/entities-api/use-create-entity";
+import { useNavigateToParent } from "../../../../hooks/router/use-navigate-to-parent";
+import { ENTITY_VIEW_STATES } from "../../../common";
+import { DriverForm } from "../DriverForm";
 
 export const CreateDriver = () => {
-  const handleSubmit = () => {
-    //poseDriver
+  const navigate = useNavigateToParent();
+  const createFunc = useCreateEntity();
+  
+  const handleSubmit = (data :{} = {}) => {
+    createFunc(data);
+    navigate();
   };
 
   return (
-    <EvoForm
-      disable={true}
-      handleSubmit={handleSubmit}
-      initialData={[]}
-      buttonText="Create"
-    />
+    <DriverForm onSubmit={handleSubmit} state={ENTITY_VIEW_STATES.create} />
   );
+
 };
--- evo-delivery-app/evo-delivery-frontend/src/pages/drivers/components/EditDriver/EditDriver.tsx
+++ evo-delivery-app/evo-delivery-frontend/src/pages/drivers/components/EditDriver/EditDriver.tsx
@@ -1,39 +1,52 @@
+import { Alert, AlertTitle, Skeleton, Stack } from "@mui/material";
 import { useGetEntity } from "../../../../hooks/entities-api/use-get-entity";
+import { useUpdateEntity } from "../../../../hooks/entities-api/use-update-entity";
 import { useEntityId } from "../../../../hooks/router/use-entity-id";
-import { EvoForm, FormFieldAttr } from "../EvoForm";
+import { useNavigateToEntityViewState } from "../../../../hooks/router/use-navigate-to-edit";
+import { ENTITY_VIEW_STATES } from "../../../common";
+import { DriverForm } from "../DriverForm";
 
 export const EditDriver = () => {
-  const driverId = useEntityId() as string;
-  const { data } = useGetEntity(driverId)
+  const driverId = useEntityId();
+  const {
+    data: { driver } = {},
+    isError,
+    isFetching,
+    isLoading,
+  } = useGetEntity(driverId as string);
 
-
+  const updateFunc = useUpdateEntity();
+  const navigateToState = useNavigateToEntityViewState();
   const handleSubmit = (data = {}) => {
-    //updateDriver
-    console.log(data);
+    updateFunc(driverId as string, data);
+    navigateToState(ENTITY_VIEW_STATES.view);
   };
 
+  if (isError) {
+    return (
+      <Alert severity="error" style={{ width: "100%", margin: 10 }}>
+        <AlertTitle>Error</AlertTitle>
+        Oh no! Something went wrong — <strong>check it out!</strong>
+      </Alert>
+    );
+  }
+
+  if (isLoading || isFetching) {
+    return (
+      <Stack spacing={1}>
+        <Skeleton variant="text" width={320} />
+        <Skeleton variant="text" width={320} />
+        <Skeleton variant="text" width={440} />
+        <Skeleton variant="rectangular" width={440} height={90} />
+      </Stack>
+    );
+  }
+
   return (
-    <EvoForm
-      disable={false}
-      initialData={[
-        {
-          label: "name",
-          type: "text",
-          value: "vladi",
-        },
-        {
-          label: "Max Distance",
-          type: "number",
-          value: "100",
-        },
-        {
-          label: "Max Capacity",
-          type: "number",
-          value: "220",
-        },
-      ]}
-      handleSubmit={handleSubmit}
-      buttonText="Update"
+    <DriverForm
+      driver={driver}
+      state={ENTITY_VIEW_STATES.edit}
+      onSubmit={handleSubmit}
     />
   );
 };
