8c90ae9b902e2830fc93383c3cf55afa4f706463
tamirho <tamirh1994@gmail.com>
2022-09-04 18:36:30 +0300

    remove axios-client

--- evo-delivery-app/evo-delivery-frontend/src/clients/axios-http-client.ts
+++
@@ -1,4 +0,0 @@
-// import { AxiosHttpClient } from '@backend/clients/AxiosHttpClient';
-// import { HttpClient } from '@backend/clients/HttpClient';
-export type tamir = {};
-// export const axiosHttpClient: HttpClient = new AxiosHttpClient();
