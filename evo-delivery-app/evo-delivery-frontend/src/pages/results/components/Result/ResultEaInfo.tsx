9deb7a3b6cb1189057c215341a9f0ca05ed3b72d
tamirho <tamirh1994@gmail.com>
2022-09-11 22:28:56 +0300

    add CircularProgress to Result page

--- evo-delivery-app/evo-delivery-frontend/src/pages/results/components/Result/ResultEaInfo.tsx
+++ evo-delivery-app/evo-delivery-frontend/src/pages/results/components/Result/ResultEaInfo.tsx
@@ -10,6 +10,7 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 import { EaResultInfo } from '@backend/types';
 import { toString } from '../../../../utils/boolean.utils';
+import { CircularProgress } from '@mui/material';
 
 export type ResultEaInfoProps = {
   info: EaResultInfo;
@@ -21,24 +22,17 @@ export const ResultEaInfo = ({ info, isDone, eaError }: ResultEaInfoProps) => {
   return (
     <Stack spacing={2} alignItems='center'>
       <Stack direction='row' spacing={2} marginTop={3}>
-        <Chip
-          label={`Fitness: ${info.fitness.toFixed(2)}`}
-          icon={<AccessibilityNewOutlinedIcon />}
-          variant='filled'
-        />
+        <Chip label={`Fitness: ${info.fitness.toFixed(2)}`} icon={<AccessibilityNewOutlinedIcon />} variant='filled' />
         <Chip label={`Time: ${new Date(info.time).getTime()}`} icon={<AccessAlarmOutlinedIcon />} variant='filled' />
         <Chip label={`Generation: ${info.generation}`} icon={<AutoModeIcon />} variant='filled' />
       </Stack>
       <Stack direction='row' spacing={2} marginBottom={3}>
         <Chip
-          label={`Done: ${toString(isDone)}`}
-          icon={<DoneAllIcon />}
-          
+          label={`Status: ${isDone ? 'Done' : 'Running'}`}
+          icon={isDone ? <DoneAllIcon /> : <CircularProgress size={20} />}
           variant='outlined'
         />
-        <Chip label={`Errors: ${toString(!!eaError)}`} 
-        icon={<ErrorOutlineIcon />} 
-        variant='outlined' />
+        <Chip label={`Errors: ${toString(!!eaError)}`} icon={<ErrorOutlineIcon />} variant='outlined' />
       </Stack>
     </Stack>
   );
